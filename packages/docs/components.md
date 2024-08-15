e969a3fea186c46600bb3b510c6290971fb5a9bd
Luis Rodriguez <luis01.ro@gmail.com>
2024-08-15 06:34:02 -0500

    chore: removing boilerplate components

--- packages/docs/components.md
+++ packages/docs/components.md
@@ -1,7 +1,4 @@
 # Components
 
-- [Loading](/components/loading)
-- [Badge](/components/badge)
-- [Tabs](/components/tabs)
 - [Button](/components/button)
 <!--do not remove - used by hygen-->
--- packages/library/src/components/LccButton/LccButton.vue
+++ packages/library/src/components/LccButton/LccButton.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <div>
+  <div class="btn btn-primary">
     Hello LccButton
   </div>
 </template>
+<style scoped>
+.btn{
+  @apply inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto
+
+  &-primary{
+    bg-blue-500 hover:bg-blue-600
+  }
+}
+</style>
