c3132b4ba5accfd4431a3e03a911dad1aabdc913
Luis Rodriguez <luis01.ro@gmail.com>
2024-09-04 07:35:16 -0500

    next button variations

--- packages/library/src/components/LccButton/LccButton.story.vue
+++ packages/library/src/components/LccButton/LccButton.story.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import LccButton from './LccButton.vue'
+import { colorsBrand, colorsState } from '../../globals'
 
-const state = reactive({})
+const state = reactive({
+  disabled: false
+})
 </script>
 <template>
-  <Story :layout="{ type: 'grid', width: '100%' }">
-    <Variant title="Variant Title">
-      <LccButton></LccButton>
+  <Story :layout="{ type: 'grid', width: '25%' }">
+    <Variant
+      v-for="c in [...colorsBrand, ...colorsState, 'ghost' as const, 'link' as const]"
+      :title="c.toUpperCase()"
+      :key="c"
+    >
+      <LccButton :color="c" :disabled="state.disabled">Click Me!</LccButton>
     </Variant>
 
-    <template #controls></template>
+    <template #controls>
+      <HstCheckbox v-model="state.disabled" title="Disabled" />
+    </template>
   </Story>
 </template>
--- packages/library/src/components/LccButton/LccButton.vue
+++ packages/library/src/components/LccButton/LccButton.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { computed, defineProps } from 'vue';
+import { ColorsBrand, ColorsState } from '../../globals';
 
+const props = defineProps<{
+  color: ColorsBrand | ColorsState | 'ghost' | 'link'
+}>()
+
+const colorClass = computed(()=>{
+  return {
+    neutral:'btn-neutral',
+    primary: 'btn-primary',
+    secondary:'btn-secondary',
+    accent:'btn-accent',
+    ghost: 'btn-ghost',
+    link: 'btn-link',
+    info:'btn-info',
+    warning:'btn-warning',
+    success:'btn-success',
+    error:'btn-error'
+  }[props.color]
+})
 </script>
 
 <template>
-  <div>
-    Hello LccButton
-  </div>
+  <button class="btn" :class="colorClass"><slot></slot></button>
 </template>
